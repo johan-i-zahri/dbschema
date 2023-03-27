f778d0ed1954d2646ebe87a31393b47f6a34b83a
johan <johan.i.zahri@gmail.com>
2023-03-27 10:46:32 +0700

    Added debug configurations and removed unnecessary command class

--- dbschema.plugin/src/org/earthling/utility/eclipseplugin/dbschema/ConfigureDBSHandler.java
+++ dbschema.plugin/src/org/earthling/utility/eclipseplugin/dbschema/ConfigureDBSHandler.java
@@ -4,8 +4,10 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 
 public class ConfigureDBSHandler extends AbstractHandler {
 
@@ -14,8 +16,14 @@ public class ConfigureDBSHandler extends AbstractHandler {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(
 				window.getShell(),
-				"dbschema.plugin",
+				"DB Schema Plugin",
 				"Hello, Eclipse world");
+		
+        Display display = PlatformUI.getWorkbench().getDisplay();
+
+        // Show a message dialog with the command name
+        MessageDialog.openInformation(display.getActiveShell(), "Command Executed", 
+        		"The command 'org.earthling.utility.eclipseplugin.dbschema.ConfigureDBSCommand' has been executed.");
 		return null;
 	}
 }
